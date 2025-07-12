4edc5a732e97898a2f3f8d69a7c6a8615197e909
Elias  Rosa <85044220+TheHunterCode@users.noreply.github.com>
2025-07-12 20:11:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function alternarModo() {
+  document.body.classList.toggle('modo-escuro');
+  const atual = document.body.classList.contains('modo-escuro') ? 'Claro' : 'Escuro';
+  document.querySelector('.btn-toggle').textContent = atual === 'Claro' ? '🌞 Modo Claro' : '🌗 Modo Escuro';
+}
